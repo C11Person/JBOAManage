29daa6da8831deca269a1157b59e053e268f5966
Leng-Mo <1026775779@qq.com>
2017-09-23 16:41:08 +0800

    更新

---
+++ JBOA/src/cn/jboa/service/LeaveService.java
@@ -0,0 +1,18 @@
+package cn.jboa.service;
+
+import java.util.List;
+
+import cn.jboa.entity.Leave;
+
+public interface LeaveService {
+	 public List<Leave> searchLeaves (Leave leave,
+				final int pageIndex, final int pageSize);
+	    public int getLeaveTotalCount(Leave leave);
+	    public Leave getLeaveById(Long id) ;
+	    public void updateLeave(Leave leave) ;
+	    public List<String> getALLtype() ;
+	    
+	    public Leave getLeaveBySn(String id);
+	    public void saveLeave(Leave leave);
+	    
+}
